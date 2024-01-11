28d996ed6fec65eca5baa94a9ccbfa85dbbba6f2
rn-santos227 <rn_santos227@yahoo.com>
2024-01-11 23:29:13 +0800

    updated api list

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -9,7 +9,42 @@ export default {
     USER: `${BACKEND}user/2`,
   },
 
+  COURSES: {
+    CREATE: `${BACKEND}teachers/courses/create`,
+    READ: `${BACKEND}teachers/courses`,
+    UPDATE: `${BACKEND}teachers/courses/`,
+    DELETE: `${BACKEND}teachers/courses/`,
+  },
+
   DASHBOARD: {
     READ: `${BACKEND}teacher/dashboard`,
+  },
+
+  RESULTS: {
+    READ: `${BACKEND}teacher/results`,
+    SEARCH: `${BACKEND}teacher/results/search`,
+    INVALIDATE: `${BACKEND}teacher/results/`,
+  },
+
+  SCHOOLS: {
+    CREATE: `${BACKEND}teachers/schools/create`,
+    READ: `${BACKEND}teachers/schools`,
+    UPDATE: `${BACKEND}teachers/schools/`,
+    DELETE: `${BACKEND}teachers/schools/`,
+  },
+
+  SECTIONS: {
+    CREATE: `${BACKEND}teachers/sections/create`,
+    READ: `${BACKEND}teachers/sections`,
+    UPDATE: `${BACKEND}teachers/sections/`,
+    DELETE: `${BACKEND}teachers/sections/`,
+  },
+
+  STUDENTS: {
+    CREATE: `${BACKEND}teachers/students/create`,
+    READ: `${BACKEND}teachers/students`,
+    UPDATE: `${BACKEND}teachers/students/`,
+    DELETE: `${BACKEND}teachers/students/`,
+    SEARCH: `${BACKEND}teachers/students/search`,
   },
 };
