87439ba8985509ca818cc880720f03286e31dd65
rn-santos227 <rn_santos227@yahoo.com>
2024-01-14 23:23:35 +0800

    update table in students page

---
+++ src/helpers/instance.ts
@@ -0,0 +1,57 @@
+import Student from "@/types/Student";
+import School from "@/types/School";
+import Section from "@/types/Section";
+import Subject from "@/types/Subject";
+import Teacher from "@/types/Teacher";
+import Question from "@/types/Question";
+import Grade from "@/types/Grade";
+
+export function getSchoolName(school: number | string | School) {
+  if (typeof school === "object" && school !== null && "name" in school) {
+    return (school as School).name;
+  }
+  return "DefaultName";
+}
+
+export function getSectionName(section: number | string | Section) {
+  if (typeof section === "object" && section !== null && "name" in section) {
+    return (section as Section).name;
+  }
+  return "DefaultName";
+}
+
+export function getSubjectName(subject: number | string | Subject) {
+  if (typeof subject === "object" && subject !== null && "name" in subject) {
+    return (subject as Subject).name;
+  }
+  return "DefaultName";
+}
+
+export function getTeacherName(teacher: number | string | Teacher) {
+  if (
+    typeof teacher === "object" &&
+    teacher !== null &&
+    "full_name" in teacher
+  ) {
+    return (teacher as Teacher).full_name;
+  }
+  return "DefaultName";
+}
+
+export function getQuestionContent(question: number | Question | undefined) {
+  if (
+    typeof question === "object" &&
+    question !== null &&
+    "content" in question
+  ) {
+    return (question as Question).content;
+  }
+  return "DefaultName";
+}
+
+export function getGradeEvaluation(grade: number | Grade | undefined) {
+  if (typeof grade === "object" && grade !== null && "evaluation" in grade) {
+    return (grade as Grade).evaluation;
+  }
+  return "DefaultName";
+}
--- src/views/students/TableView.vue
+++ src/views/students/TableView.vue
@@ -3,29 +3,71 @@
     <v-card variant="flat">
       <v-row class="mt-1">
         <v-spacer />
-        <v-text-field
-          class="mr-4"
-          v-model="search"
-          label="Search Student"
-          density="compact"
-          variant="outlined"
-        />
-        <v-btn
-          class="ml-4"
-          variant="outlined"
-          color="purple-darken-3"
-          prepend-icon="mdi-refresh"
-          height="40"
-          width="180"
-          @click.prevent="resetSearch"
-        >
-          Reset List
-        </v-btn>
+        <v-col class="d-flex">
+          <v-text-field
+            class="mr-4"
+            v-model="search"
+            label="Search Student"
+            density="compact"
+            variant="outlined"
+          />
+          <v-btn
+            class="ml-4"
+            variant="outlined"
+            color="purple-darken-3"
+            prepend-icon="mdi-refresh"
+            height="40"
+            width="180"
+            @click.prevent="resetSearch"
+          >
+            Reset List
+          </v-btn>
+        </v-col>
       </v-row>
     </v-card>
   </v-card-text>
   <v-divider />
-  <v-card-text class="table-height"> </v-card-text>
+  <v-card-text class="table-height">
+    <v-data-table
+      class="w-100"
+      item-value="name"
+      :search="search"
+      :items="students"
+      :headers="headers"
+      :items-per-page="useStudentsModule().page.per_page"
+      :loading="useStudentsModule().isTableLoading"
+      :page="useStudentsModule().page.current_page"
+    >
+      <template v-slot:item="{ item }">
+        <tr>
+          <td class="text-left">
+            {{ item.full_name }}
+          </td>
+          <td class="text-left">
+            {{ getSectionName(item.section) }}
+          </td>
+          <td class="text-left">
+            {{ item.email }}
+          </td>
+          <td class="text-left">
+            {{ getSchoolName(item.school) }}
+          </td>
+          <td>
+            <v-btn block color="purple-darken-3" variant="outlined">
+              <v-icon size="large">mdi-dots-horizontal</v-icon>
+              <v-menu activator="parent">
+                <v-list density="compact" variant="plain">
+                  <UpdateView v-bind:student="item" />
+                  <DeleteView v-bind:student="item" />
+                </v-list>
+              </v-menu>
+            </v-btn>
+          </td>
+        </tr>
+      </template>
+      <template v-slot:bottom> </template>
+    </v-data-table>
+  </v-card-text>
   <v-divider />
   <v-card-actions class="mt-auto pa-4 mb-12">
     <v-spacer />
@@ -48,6 +90,7 @@
 <script setup lang="ts">
 import { computed, onMounted, provide, ref, watch } from "vue";
 import { useStudentsModule } from "@/store";
+import { getSchoolName, getSectionName } from "@/helpers/instance";
 
 import SuccessDialogComponent from "@/components/dialogs/SuccessDialogComponent.vue";
 import ErrorDialogComponent from "@/components/dialogs/ErrorDialogComponent.vue";
