2f09be7065dba9e0ff4381c1adfb3ab7f9a2b7a3
rn-santos227 <rn_santos227@yahoo.com>
2024-01-15 20:55:42 +0800

    added read function in courses module

--- src/store/modules/courses_module.ts
+++ src/store/modules/courses_module.ts
@@ -1,5 +1,8 @@
 import Course from "@/types/Course";
+import api from "@/helpers/api";
+
 import { defineStore } from "pinia";
+import { authenticatedFetch } from "@/services/api";
 
 export const useCoursesModule = defineStore("courses", {
   state: () => ({
@@ -26,6 +29,22 @@ export const useCoursesModule = defineStore("courses", {
 
     deleteCourse(course: Course): void {
       this.courses = this.courses.filter((item) => item.id !== course.id);
+    },
+
+    async read(): Promise<boolean> {
+      try {
+        this.isTableLoading = true;
+        const response = await authenticatedFetch(api.COURSES.READ);
+        const data = await response.json();
+        const { courses } = data;
+        this.setCourses(courses);
+        return true;
+      } catch (error) {
+        console.error("Error Course in:", error);
+        return false;
+      } finally {
+        this.isTableLoading = false;
+      }
     },
   },
 
