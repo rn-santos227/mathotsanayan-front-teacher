c7d70470d5b8ffb70b759d0e395d068914b842fa
rn-santos227 <rn_santos227@yahoo.com>
2024-01-24 22:55:15 +0800

    update help panel

--- src/views/help/DialogView.vue
+++ src/views/help/DialogView.vue
@@ -6,11 +6,38 @@
     @click="dialog = true"
   />
 
-  <v-dialog v-model="dialog" width="auto"> </v-dialog>
+  <v-dialog class="ma-auto" width="70%" persistent v-model="dialog">
+    <v-card>
+      <v-card class="rounded-0 rounded-t py-2" color="purple-darken-3" flat>
+        <v-card-actions class="mx-4">
+          <span class="text-h6"> Help Library </span>
+          <v-spacer />
+          <v-btn
+            density="comfortable"
+            variant="outlined"
+            icon="mdi-close"
+            @click="close"
+          >
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card-text class="question-height"> </v-card-text>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 
 const dialog = ref<boolean>(false);
+const close = () => {
+  dialog.value = !dialog.value;
+};
 </script>
+
+<style scoped>
+.question-height {
+  height: calc(100vh - 200px);
+  overflow-y: auto;
+}
+</style>
