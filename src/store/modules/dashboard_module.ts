6e4dd650ef08fdb83fefd6a44b1103457ad4f7f3
rn-santos227 <rn_santos227@yahoo.com>
2024-01-28 20:42:20 +0800

    update dashboard module

--- src/store/modules/dashboard_module.ts
+++ src/store/modules/dashboard_module.ts
@@ -1,4 +1,6 @@
 import Dashboard from "@/interfaces/Dashboard";
+import GraphPie from "@/interfaces/GraphPie";
+import ResultModule from "@/interfaces/ResultModule";
 import api from "@/helpers/api";
 
 import { defineStore } from "pinia";
@@ -8,11 +10,21 @@ export const useDashboardModule = defineStore("dashboard", {
   state: () => ({
     isLoading: false as boolean,
     dashboard: {} as Dashboard,
+    graphPie: {} as GraphPie,
+    resultModules: [] as ResultModule[],
   }),
 
   actions: {
     setDashboard(dashboard: Dashboard): void {
       this.dashboard = dashboard;
+    },
+
+    setGraphPie(graphPie: GraphPie): void {
+      this.graphPie = graphPie;
+    },
+
+    setResultModules(resultModules: ResultModule[]) {
+      this.resultModules = resultModules;
     },
 
     async read() {
@@ -23,7 +35,17 @@ export const useDashboardModule = defineStore("dashboard", {
     },
 
     async ratio() {
-      authenticatedFetch(api.DASHBOARD.RATIO);
+      const response = await authenticatedFetch(api.DASHBOARD.RATIO);
+      const data = await response.json();
+      const { graph } = data;
+      this.setGraphPie(graph);
+    },
+
+    async modules() {
+      const response = await authenticatedFetch(api.DASHBOARD.MODULE);
+      const data = await response.json();
+      const { result_modules } = data;
+      this.setResultModules(result_modules);
     },
   },
 
