f4b214d034e652494ca591ad1d790a24dc614583
rn-santos227 <rn_santos227@yahoo.com>
2024-02-03 21:46:46 +0800

    added pages file for help panel

---
+++ src/views/help/documentation/DocIntroView.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card-text>
+    <span class="text-h5 font-weight-bold">MatHotSanayan CAA Introduction</span>
+    <v-divider class="my-2" />
+    <p class="ma-6">
+      MatHotSanayan is a computer aided assessment (CAA) tool that aims to
+      expose you, the student, to non-routine problems (NRP) to enhance your
+      higher order of thinking skills (HOTS). This serves as a practice item
+      test bank in NRP that covers seletected topics in Mathematics in the
+      Modern World (MMW), such as mathematical language and symbols, and problem
+      solving reasoning.
+    </p>
+
+    <p class="ma-6">
+      This is composed of four modules. The first module focuses on mathematical
+      language and symbols, while the second to fourth modules focus on problem
+      solving and reasoning. Teach module consists of 30 NRP that will appear at
+      random.
+    </p>
+  </v-card-text>
+</template>
