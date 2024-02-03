b790670fa31db55ecd44c1c1306835193ae4f2e1
rn-santos227 <rn_santos227@yahoo.com>
2024-02-03 21:55:23 +0800

    update audit, courses, dashboard, and introduction page in help panel

---
+++ src/views/help/documentation/DocDashboardView.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card-text>
+    <span class="text-h5 font-weight-bold">
+      MatHotSanayan Teacher Dashboard
+    </span>
+    <v-divider class="my-2" />
+  </v-card-text>
+</template>
+
+<script setup lang="ts">
+import docs from "@/helpers/documentation/doc_dashboard";
+</script>
