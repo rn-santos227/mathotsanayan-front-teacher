f72c85e3abfaeefaff828fd923e1cfbc3f5a4f81
rn-santos227 <rn_santos227@yahoo.com>
2024-02-12 23:22:04 +0800

    update schools page in help panel

--- src/views/help/documentation/DocSchoolsView.vue
+++ src/views/help/documentation/DocSchoolsView.vue
@@ -1,12 +1,35 @@
 <template>
   <v-card-text>
-    <span class="text-h5 font-weight-bold">
-      MatHotSanayan Teacher Schools
-    </span>
-    <v-divider class="my-2" />
-    <p class="ma-6">
-      {{ docs[0] }}
-    </p>
+    <div v-if="focusPage === 'schools-main'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Teacher Schools Table
+      </span>
+      <v-divider class="my-2" />
+      <p class="ma-6">
+        {{ docs[0] }}
+      </p>
+    </div>
+
+    <div v-if="focusPage === 'schools-create'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Teacher Schools Create
+      </span>
+      <v-divider class="my-2" />
+    </div>
+
+    <div v-if="focusPage === 'schools-update'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Teacher Schools Update
+      </span>
+      <v-divider class="my-2" />
+    </div>
+
+    <div v-if="focusPage === 'schools-delete'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Teacher Schools Deletion
+      </span>
+      <v-divider class="my-2" />
+    </div>
   </v-card-text>
 </template>
 
