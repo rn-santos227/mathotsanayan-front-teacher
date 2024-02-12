eed4b9fdf9f6828599e73b2c524a8e812bce1c9e
rn-santos227 <rn_santos227@yahoo.com>
2024-02-12 23:23:41 +0800

    update section page in help panel

--- src/views/help/documentation/DocSectionsView.vue
+++ src/views/help/documentation/DocSectionsView.vue
@@ -1,12 +1,35 @@
 <template>
   <v-card-text>
-    <span class="text-h5 font-weight-bold">
-      MatHotSanayan Teacher Sections
-    </span>
-    <v-divider class="my-2" />
-    <p class="ma-6">
-      {{ docs[0] }}
-    </p>
+    <div v-if="focusPage === 'section-main'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Teacher Section Table
+      </span>
+      <v-divider class="my-2" />
+      <p class="ma-6">
+        {{ docs[0] }}
+      </p>
+    </div>
+
+    <div v-if="focusPage === 'section-create'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Teacher Section Create
+      </span>
+      <v-divider class="my-2" />
+    </div>
+
+    <div v-if="focusPage === 'section-update'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Teacher Section Update
+      </span>
+      <v-divider class="my-2" />
+    </div>
+
+    <div v-if="focusPage === 'section-delete'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Teacher Section Deletion
+      </span>
+      <v-divider class="my-2" />
+    </div>
   </v-card-text>
 </template>
 
