7dc8826d395f670d595871ae6066168d301be61e
rn-santos227 <rn_santos227@yahoo.com>
2024-02-12 23:27:55 +0800

    update student page in help panel

--- src/views/help/documentation/DocStudentsVue.vue
+++ src/views/help/documentation/DocStudentsVue.vue
@@ -1,12 +1,42 @@
 <template>
   <v-card-text>
-    <span class="text-h5 font-weight-bold">
-      MatHotSanayan Teacher Student
-    </span>
-    <v-divider class="my-2" />
-    <p class="ma-6">
-      {{ docs[0] }}
-    </p>
+    <div v-if="focusPage === 'student-main'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Teacher Teacher Table
+      </span>
+      <v-divider class="my-2" />
+      <p class="ma-6">
+        {{ docs[0] }}
+      </p>
+    </div>
+
+    <div v-if="focusPage === 'student-search'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Teacher Teacher Filter Search
+      </span>
+      <v-divider class="my-2" />
+    </div>
+
+    <div v-if="focusPage === 'student-create'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Teacher Teacher Create
+      </span>
+      <v-divider class="my-2" />
+    </div>
+
+    <div v-if="focusPage === 'student-update'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Teacher Teacher Update
+      </span>
+      <v-divider class="my-2" />
+    </div>
+
+    <div v-if="focusPage === 'student-delete'">
+      <span class="text-h5 font-weight-bold">
+        MatHotSanayan Teacher Teacher Deletion
+      </span>
+      <v-divider class="my-2" />
+    </div>
   </v-card-text>
 </template>
 
