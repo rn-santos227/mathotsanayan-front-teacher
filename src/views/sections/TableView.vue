b7ec0df48760ac3533385cad50659d176ec7084c
rn-santos227 <rn_santos227@yahoo.com>
2024-02-13 22:54:55 +0800

    fixed search placeholder in sections

--- src/views/sections/TableView.vue
+++ src/views/sections/TableView.vue
@@ -5,7 +5,7 @@
       <v-text-field
         class="mr-4"
         v-model="search"
-        label="Search Student"
+        label="Search Section"
         density="compact"
         variant="outlined"
       />
