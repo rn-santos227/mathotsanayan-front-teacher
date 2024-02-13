e188cc25f75d84a820bb6b81bf1268b763d6e4cc
rn-santos227 <rn_santos227@yahoo.com>
2024-02-13 21:57:01 +0800

    update doc introduction

--- src/helpers/documentation/doc_introduction.ts
+++ src/helpers/documentation/doc_introduction.ts
@@ -1,3 +1,32 @@
-const docs: string[] = [];
+const docs: string[] = [
+  `Welcome to MatHOTSanayan, the dedicated platform for managing Computer
+  Aided Assessment (CAA) tasks, tailored specifically for teachers. In this
+  guide, we'll explore the core functionalities designed to empower
+  educators in efficiently overseeing their classroom activities. The
+  teacher dashboard serves as a central hub, offering real-time insights
+  into examination progress and results of students assigned to them. With
+  an intuitive interface, teachers can seamlessly navigate through vital
+  performance indicators, ensuring informed decision-making throughout their
+  teaching journey.`,
+
+  `Within Student Management, MatHOTSanayan equips teachers with tools for
+  seamless enrollment and organization of students within their classes.
+  Teachers can effortlessly add new students, sections, courses, and
+  schools, ensuring accurate and up-to-date records. Notably, when teachers
+  create new sections, they are automatically assigned to oversee them,
+  streamlining administrative processes. Furthermore, teachers have the
+  authority to assign students to the sections they handle, enabling
+  personalized classroom management aligned with their teaching
+  responsibilities.`,
+
+  `MatHOTSanayan also provides robust features for Examination Progress and
+  Result tracking, empowering teachers to monitor student performance
+  effectively. Through the platform, teachers gain access to detailed
+  insights into the progress and results of students assigned to their
+  sections, facilitating targeted intervention and support as needed. This
+  section of the guide offers comprehensive instructions on how teachers can
+  leverage these features to enhance their teaching practices and ensure
+  academic success for their students.`,
+];
 
 export default docs;
