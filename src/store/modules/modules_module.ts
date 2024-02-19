98a576714de0e3d326ff5bae7e2402c36a9049f8
rn-santos227 <rn_santos227@yahoo.com>
2024-02-19 23:11:34 +0800

    added read function in modules module

--- src/store/modules/modules_module.ts
+++ src/store/modules/modules_module.ts
@@ -1,7 +1,9 @@
 import Module from "@/types/Module";
 import Page from "@/interfaces/Page";
+import api from "@/helpers/api";
 
 import { defineStore } from "pinia";
+import { authenticatedFetch } from "@/services/api";
 
 export const useModulesModule = defineStore("modules", {
   state: () => ({
@@ -10,6 +12,43 @@ export const useModulesModule = defineStore("modules", {
     isLoading: false,
     isTableLoading: false,
   }),
+
+  actions: {
+    setModules(modules: Module[]) {
+      this.modules = modules;
+    },
+
+    setPage(page: Page) {
+      this.page = page;
+    },
+
+    async read(page = 1): Promise<boolean> {
+      try {
+        this.isTableLoading = true;
+        const response = await authenticatedFetch(
+          `${api.MODULES.READ}?page=${page}`
+        );
+        const res = await response.json();
+        const { data } = res.modules;
+
+        if (data) {
+          data.forEach((item: Module) => {
+            if (item.active) item.active = true;
+            else item.active = false;
+          });
+        }
+
+        this.setPage(res.modules);
+        this.setModules(data);
+        return true;
+      } catch (error) {
+        console.error("Error Module in:", error);
+        return false;
+      } finally {
+        this.isTableLoading = false;
+      }
+    },
+  },
 
   getters: {
     getModules(): Module[] {
