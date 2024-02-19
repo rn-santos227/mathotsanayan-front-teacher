f3c14a8e72b328b72782ecbf76bf5c25ba467a41
rn-santos227 <rn_santos227@yahoo.com>
2024-02-19 23:21:14 +0800

    added modules and questions headers

---
+++ src/helpers/headers/header_modules.ts
@@ -0,0 +1,31 @@
+import type { VDataTable } from "vuetify/lib/components/index.mjs";
+type ReadonlyHeaders = InstanceType<typeof VDataTable>["headers"];
+const headers: ReadonlyHeaders = [
+  {
+    title: "Module Name",
+    align: "start",
+    sortable: true,
+    key: "name",
+  },
+  {
+    title: "Number of Items",
+    align: "start",
+    sortable: true,
+    key: "questions.length",
+  },
+  {
+    title: "Module Subject",
+    align: "start",
+    sortable: true,
+    key: "subject.name",
+  },
+  {
+    title: "Action",
+    align: "end",
+    sortable: false,
+    key: "action",
+    width: "10%",
+  },
+];
+
+export default headers;
---
+++ src/helpers/headers/header_questions.ts
@@ -0,0 +1,44 @@
+import type { VDataTable } from "vuetify/lib/components/index.mjs";
+type ReadonlyHeaders = InstanceType<typeof VDataTable>["headers"];
+const headers: ReadonlyHeaders = [
+  {
+    title: "Image",
+    align: "start",
+    key: "image",
+    sortable: false,
+  },
+  {
+    title: "Question",
+    align: "start",
+    sortable: true,
+    key: "content",
+    width: "45%",
+  },
+  {
+    title: "Question Type",
+    align: "start",
+    sortable: true,
+    key: "type",
+  },
+  {
+    title: "Correct Answers",
+    align: "start",
+    sortable: true,
+    key: "corrects.length",
+  },
+  {
+    title: "Options",
+    align: "start",
+    sortable: true,
+    key: "options.length",
+  },
+  {
+    title: "Action",
+    align: "end",
+    sortable: false,
+    key: "action",
+    width: "10%",
+  },
+];
+
+export default headers;
