2295a62ae7cab32a1c7bfe530e7730eb951b1d20
rn-santos227 <rn_santos227@yahoo.com>
2024-02-23 22:14:26 +0800

    update router index

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,6 @@ import CoursesView from "../views/courses/MainView.vue";
 import DashboardView from "../views/dashboard/MainView.vue";
 import ModuleView from "../views/modules/MainView.vue";
 import ReportsView from "../views/reports/MainView.vue";
-import SchoolsView from "../views/schools/MainView.vue";
 import SectionsView from "../views/sections/MainView.vue";
 import StudentsView from "../views/students/MainView.vue";
 
@@ -48,14 +47,6 @@ const routes: Array<RouteRecordRaw> = [
     path: "/reports",
     name: "reports",
     component: ReportsView,
-    meta: {
-      forAuth: true,
-    },
-  },
-  {
-    path: "/schools",
-    name: "schools",
-    component: SchoolsView,
     meta: {
       forAuth: true,
     },
