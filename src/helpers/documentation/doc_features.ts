8df738ae3c618eeccc595101df09a3661caf105a
rn-santos227 <rn_santos227@yahoo.com>
2024-02-28 23:52:10 +0800

    update features page in helper page

--- src/helpers/documentation/doc_features.ts
+++ src/helpers/documentation/doc_features.ts
@@ -1,6 +1,20 @@
 const docs: string[] = [
   ` The interface is intuitive and easy to navigate, allowing teachers
   to quickly access the test bank and various features.`,
+
+  ` The test bank covers a wide range of topics and concepts relevant
+  to the subject area or level being taught. It includes questions of
+  varying difficulty levels to cater to the diverse needs of
+  students.`,
+
+  ` By stimulating higher-order thinking skills, such as analysis,
+  evaluation, and application, the practice tests encourage deeper
+  understanding and mastery of mathematical concepts rather than mere
+  rote memorization.`,
+
+  ` Generating randomized problems prevents students from relying on
+  memorization and encourages them to develop a thorough understanding
+  of concepts, promoting true mastery of the material.`,
 ];
 
 export default docs;
