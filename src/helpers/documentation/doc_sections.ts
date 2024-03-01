fddc4d597f383b8bce1c5698014fe32289ceef89
rn-santos227 <rn_santos227@yahoo.com>
2024-03-01 21:12:03 +0800

    added section delete page in help panel

--- src/helpers/documentation/doc_sections.ts
+++ src/helpers/documentation/doc_sections.ts
@@ -1,18 +1,26 @@
 const docs: string[] = [
-  `        This serves as a database that stores information about the students’
+  `This serves as a database that stores information about the students’
   section and the number of students per section. It authorizes teachers
   with a user-friendly interface to efficiently create, update, and delete
   sections.`,
 
-  `        This functionality allows teachers to create or add new sections to the
+  `This functionality allows teachers to create or add new sections to the
   database. It involves providing information such as student’s section,
   school, and section description.`,
 
-  `        This functionality enables teachers to modify existing section details
+  `This functionality enables teachers to modify existing section details
   stored in the database. For example, they might need to update the
   student’s section, school, and section description.`,
 
-  ``,
+  `This functionality permits teachers to remove sections from the
+  database. Given the potential impact of deletion, particularly if
+  performed inadvertently, this action necessitates confirmation or
+  verification to guarantee its deliberate execution. This typically
+  entails displaying a pop-up dialog prompting the teacher to affirm their
+  intention to delete the section. Upon confirmation, the record
+  corresponding to the section is eliminated from the Sections Manager,
+  and any linked data may be subject to deletion or modification based on
+  the configuration of the database.`,
 ];
 
 export default docs;
--- src/views/help/documentation/DocSectionsView.vue
+++ src/views/help/documentation/DocSectionsView.vue
@@ -56,6 +56,12 @@
       <p class="ma-6">
         {{ docs[3] }}
       </p>
+
+      <v-img
+        class="ma-6"
+        cover
+        :src="require('@/assets/images/help/doc_012.png')"
+      />
     </div>
   </v-card-text>
 </template>
